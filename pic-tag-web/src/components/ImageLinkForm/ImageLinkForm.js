3ff30adf25bf4b9df02970436a69ed857cb37d72
Ray Yuan <ray.x.yuan@gmail.com>
2018-01-14 00:08:46 -0800

    more css for form

--- pic-tag-web/src/components/ImageLinkForm/ImageLinkForm.js
+++ pic-tag-web/src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm=()=>{
         {'This Magic Brain will process your picture. Give it a try!'}
       </p>
       <div className='center'>
-        <div className='center pa4 br3 shadow-5'>
+        <div className='form center pa4 br3 shadow-5'>
           <input className='f4 pa2 w-70 center' type='tex'/>
           <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Apply</button>
         </div>
