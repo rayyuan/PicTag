9c17a8fefe4b3bcdd09f668687fb5b67b8f58a42
Ray Yuan <ray.x.yuan@gmail.com>
2018-01-13 23:58:41 -0800

    button in form

--- pic-tag-web/src/components/ImageLinkForm/ImageLinkForm.js
+++ pic-tag-web/src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,6 +6,12 @@ const ImageLinkForm=()=>{
       <p className='f3'>
         {'This Magic Brain will process your picture. Give it a try!'}
       </p>
+      <div className='center'>
+        <div className='center pa4 br3 shadow-5'>
+          <input className='f4 pa2 w-70 center' type='tex'/>
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light purple'>Apply</button>
+        </div>
+      </div>
     </div>
 
   );
