b0628f18b0d09eb16370604cb7822946b050b55c
Ray Yuan <ray.x.yuan@gmail.com>
2018-01-14 02:55:54 -0800

    imported clarifai api

--- pic-tag-web/src/App.js
+++ pic-tag-web/src/App.js
@@ -5,6 +5,11 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 import './App.css';
+import Clarifai from 'clarifai';
+
+const app=new Clarifai.App({
+  apiKey:'b817023b2ce544e883b431fb61e4fb04'
+});
 
 
 class App extends Component {
@@ -17,13 +22,24 @@ class App extends Component {
   onInputChange=(event)=>{
     this.setState({input:event.target.value});
   }
+  onSubmit=()=>{
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log('resp');
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange}/>
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+        <Recognition/>
       </div>
     );
   }
--- pic-tag-web/src/components/ImageLinkForm/ImageLinkForm.js
+++ pic-tag-web/src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 //props destructured
-const ImageLinkForm=({onInputChange})=>{
+const ImageLinkForm=({onInputChange,onSubmit})=>{
   return(
     <div>
       <p className='f3'>
@@ -11,7 +11,7 @@ const ImageLinkForm=({onInputChange})=>{
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
           <input onChange={onInputChange} className='f4 pa2 w-70 center' type='tex'/>
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Apply</button>
+          <button onClick={onSubmit} className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Apply</button>
         </div>
       </div>
     </div>
