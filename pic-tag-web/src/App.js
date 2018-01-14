0e50cdc9d9a6465b722d0a1c7f292559bbd5c30f
Ray Yuan <ray.x.yuan@gmail.com>
2018-01-13 23:56:17 -0800

    image link form text

--- pic-tag-web/src/App.js
+++ pic-tag-web/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
-import ImageLinkForm from './component/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 class App extends Component {
--- pic-tag-web/src/components/ImageLinkForm/ImageLinkForm.js
+++ pic-tag-web/src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const ImageLinkForm=()=>{
   return(
-    <nav style={{display:'flex',justifyContent:'flex-end'}}>
-      <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-    </nav>
+    <div>
+      <p className='f3'>
+        {'This Magic Brain will process your picture. Give it a try!'}
+      </p>
+    </div>
+
   );
 }
 
