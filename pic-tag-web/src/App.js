73044f39e516c07a309cd5c91ff91cfc0de2feeb
Ray Yuan <ray.x.yuan@gmail.com>
2018-01-14 00:30:31 -0800

    finished design

--- pic-tag-web/src/App.js
+++ pic-tag-web/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +12,9 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
+        {}
       </div>
     );
   }
