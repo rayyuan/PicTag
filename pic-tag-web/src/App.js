9e3b0349939baa9297a833f1908969d44bafd0d8
Ray Yuan <ray.x.yuan@gmail.com>
2018-01-14 17:23:46 -0800

    creating bounding box from clarifai return data

--- pic-tag-web/src/App.js
+++ pic-tag-web/src/App.js
@@ -17,21 +17,42 @@ class App extends Component {
     super()
     this.state={
       input:'',
-      imageUrl:''
+      imageUrl:'',
+      box:{}
     }
   }
   onInputChange=(event)=>{
     this.setState({input:event.target.value});
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+
+
   onSubmit=()=>{
     this.setState({imageUrl:this.state.input});
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
     function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      console.log(this.displayFaceBox(this.calculateFaceLocation(response)));
     },
     function(err) {
-      // there was an error
+      console.log(err);
     }
   );
   }
